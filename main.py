7d7695c0c3c2dc0c51e88d51768ad7f64824981d
roma <bshs71734@gmail.com>
2024-10-31 15:06:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-#декоратор
-def select(input_func):    
-    def output_func(index): 
-        print(f"********Книга {index + 1}*********")  # выводим номер книги
-        input_func(index)  
-        print(f"********{books['year'][index]}*********")  # выводим год
-    return output_func  # возвращаем новую функцию
-
 #списки для словаря 
 title = ['Война и мир', 'Дом, в котором', '1984', 'Гарри Поттер и философский камень', 'Мастер и Маргарита']
 author = ['Лев Толстой', 'Мариам Петросян', 'Джордж Оруэлл', 'Дж.К. Роулинг', 'Михаил Булгаков']
@@ -14,10 +6,9 @@ year = ['1985', '1573', '2145', '4525', '1998']
 #Словарь
 books = {'title': title, 'author': author, 'year': year}
 
-# перебор все книги и использеум декоратор
+# перебор все книги и использеум 
 for index in range(len(title)):
-    @select
-    def output(index): 
-        print(f"Название книги: {books['title'][index]}, Автор: {books['author'][index]}")
-
-    output(index) 
+    print(f"********Книга {index + 1}*********")
+    print(f"Название:{books['title'][index]},Автор: {books['author'][index]}")
+    print(f"********{books['year'][index]}*********")  # выводим год
+    print()
